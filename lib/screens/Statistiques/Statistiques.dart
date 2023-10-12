2db2b62ac87084225b7948bc08045295f7e9aea7
Amadou Touré <Amadouit223@gmail.com>
2023-10-12 12:12:07 +0000

    Le choix du graphique

--- lib/screens/Statistiques/Statistiques.dart
+++ lib/screens/Statistiques/Statistiques.dart
@@ -10,9 +10,9 @@ class Statistiques extends StatelessWidget {
       body: SingleChildScrollView(
         child: Center(
           child: Container(
-            margin: const EdgeInsets.only(top:35),
+            margin: const EdgeInsets.only(top:150),
             width: 370,
-            height: 787,
+            height: 500,
             decoration: ShapeDecoration(
               color: const Color(0xFFFFFBFB),
               shape: RoundedRectangleBorder(
@@ -29,34 +29,22 @@ class Statistiques extends StatelessWidget {
             ),
             child: Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 18,right: 18,top: 15),
-                  width: 372,
-                  height: 370,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFFFFBFB),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(28),
-                    ),
-                    shadows: const [
-                      BoxShadow(
-                        color: Color(0x70000000),
-                        blurRadius: 13,
-                        offset: Offset(1, 1),
-                        spreadRadius: 0,
-                      )
-                    ],
-                  ),
-                  child: Center(
+                Center(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Column(
                           children: [
-                            const Text(
-                              "LES DEPENSES PAR CATEGORIES"
+                            Container(
+                              margin: const EdgeInsets.only(top: 30),
+                              child: const Text(
+                                "LES DEPENSES PAR CATEGORIES",
+                                style: TextStyle(
+                                  fontSize: 20
+                                ),
+                              ),
                             ),
-                            const SizedBox(height: 15),
+                            const SizedBox(height: 50),
                             SizedBox(
                               height: 300,
                               child: Stack(
@@ -122,7 +110,7 @@ class Statistiques extends StatelessWidget {
                             ),
                           ],
                         ),
-                        const SizedBox(height: 15),
+                        const SizedBox(height: 25),
                         Container(
                           margin: const EdgeInsets.only(bottom: 8),
                           child: Row(
@@ -138,112 +126,6 @@ class Statistiques extends StatelessWidget {
                       ],
                     ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 18,right: 18,top: 15),
-                  width: 372,
-                  height: 370,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFFFFBFB),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(28),
-                    ),
-                    shadows: const [
-                      BoxShadow(
-                        color: Color(0x70000000),
-                        blurRadius: 13,
-                        offset: Offset(1, 1),
-                        spreadRadius: 0,
-                      )
-                    ],
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            const Text('LES DEPENSES PAR CATEGORIES'),
-                            const SizedBox(height: 15),
-                            SizedBox(
-                              height: 300,
-                              child: Stack(
-                                children: [
-                                  PieChart(
-                                    PieChartData(
-                                      startDegreeOffset: 90,
-                                      sectionsSpace: 0.5,
-                                      centerSpaceRadius: 100,
-                                      sections: [
-                                        PieChartSectionData(
-                                          value: 45,
-                                          color: Colors.grey,
-                                          radius: 55,
-                                          showTitle: false,
-                                        ),
-                                        PieChartSectionData(
-                                          value: 25,
-                                          color: Colors.blue,
-                                          radius: 55,
-                                          showTitle: false,
-                                        ),
-                                        PieChartSectionData(
-                                          value: 35,
-                                          color: Colors.amberAccent,
-                                          radius: 55,
-                                          showTitle: false,
-                                        ),
-                                        PieChartSectionData(
-                                          value: 15,
-                                          color: Colors.pink,
-                                          radius: 55,
-                                          showTitle: false,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Positioned.fill(
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Container(
-                                          height: 160,
-                                          width: 160,
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            shape: BoxShape.circle,
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey.shade200,
-                                                blurRadius: 10.0,
-                                                spreadRadius: 10.0,
-                                                offset: const Offset(3.0, 3.0),
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            _buildLegendItem("Nourriture", Colors.green.shade900),
-                            _buildLegendItem("Loyer", Colors.blue),
-                            _buildLegendItem("Loisir", Colors.pink),
-                            _buildLegendItem("Transport", Colors.amberAccent),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
